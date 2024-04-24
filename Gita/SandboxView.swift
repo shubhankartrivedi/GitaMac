0a825b35d9774562fa62843efb14f3a34b57eb16
Shubhankar <shubhankartrivedi1234@gmail.com>
2024-04-25 03:09:38 +0530

    init

---
+++ Gita/SandboxView.swift
@@ -0,0 +1,18 @@
+//
+//  SandboxView.swift
+//  Gita
+//
+//  Created by Shubhankar Trivedi on 25/04/24.
+//
+
+import SwiftUI
+
+struct SandboxView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SandboxView()
+}
---
+++ Gita/SidebarView.swift
@@ -0,0 +1,18 @@
+//
+//  SidebarView.swift
+//  Gita
+//
+//  Created by Shubhankar Trivedi on 25/04/24.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SidebarView()
+}
