f28adee4066005879b388b51d7414ce040f61b24
Shubhankar <shubhankartrivedi1234@gmail.com>
2024-04-25 01:39:13 +0530

    Initial Commit

---
+++ Gita/GitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitaApp.swift
+//  Gita
+//
+//  Created by Shubhankar Trivedi on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
